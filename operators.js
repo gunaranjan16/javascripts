6cfebedd9a40518d1aaa4b432f4f65ef9933fec2
GUNARANJAN <gunaranjan1610@gmail.com>
2024-01-21 23:33:39 +0530

    Initial commit

---
+++ operators.js
@@ -0,0 +1,26 @@
+//Assignment Operator
+
+var a= 10
+console.log("Assignment Operator Output:",a)
+
+//Arithmetic operator-> +,-,*,/,%
+var aA =10
+var bA = 10
+console.log(typeof aA)
+console.log("Arithmetic Operator Output Addition: ",aA + bA)
+console.log("Arithmetic Operator Output Subtraction: ",aA - bA)
+console.log("Arithmetic Operator Output Multiplication: ",aA * bA)
+console.log("Arithmetic Operator Output division: ",aA / bA)
+console.log("Arithmetic Operator Output modulus: ",aA % bA)
+
+
+//Logical operators-> &&,||,!
+
+var aL =1
+var bL = 10
+console.log("Logical Operator && Output Addition: ",aL && bL)//AND - CHECK BOTH NOS ANS 2ND NO (ONLY IF 1ST NO IS 0 OP = 0)
+console.log("Logical Operator || Output Addition: ",aL || bL)//OR - OP IS FIRST NO.. ONLY IF 1ST NO. IS 0 2ND IS OP
+console.log("Logical Operator ! Output Addition: ", !bL)
+console.log(100||20&&0)//ANS 100
+console.log((100||20)&&0)//ANS 0
+console.log(!-19)//ANY NUMBER OTHER THAN = 0 FALSE
---
+++ variable.js
@@ -0,0 +1,10 @@
+a='hello KEC!'
+console.log("1.",a)
+var b='var'
+console.log("This variable belongs to var type value is:",b)
+let c='let'
+console.log("This variable belongs to let type value is:",c)
+const d= 100
+console.log("This variable belongs to var type value is:",d)
+d= 200
+console.log("This variable belongs to var type value is:",d)
